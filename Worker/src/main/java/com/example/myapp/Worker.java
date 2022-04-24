058b627f7be39cc315c0b28776956b749ea3868e
Ron Levi <t-ronlevi@microsoft.com>
2022-04-24 20:45:26 +0300

    test2

--- Worker/src/main/java/com/example/myapp/Worker.java
+++ Worker/src/main/java/com/example/myapp/Worker.java
@@ -60,10 +60,10 @@ public class Worker {
         CreateQueueResponse createQueueResponse = sqs.createQueue(CreateQueueRequest.builder().queueName("input-1").build());
         sqs.createQueue(CreateQueueRequest.builder().queueName("output-1").build());
         Map<String, MessageAttributeValue> tempMap = new HashMap<>();
-        tempMap.put("bucket", MessageAttributeValue.builder().stringValue("dspbucket12345").build());
+        tempMap.put("bucket", MessageAttributeValue.builder().stringValue("dspbucket12345").dataType("String").build());
         tempMap.put("analysis-type", MessageAttributeValue.builder().stringValue("CONSTITUENCY").build());
         tempMap.put("url", MessageAttributeValue.builder().stringValue("https://www.gutenberg.org/files/1659/1659-0.txt").build());
-        sqs.sendMessage(SendMessageRequest.builder().queueUrl(createQueueResponse.queueUrl()).messageBody("").messageAttributes(tempMap).build());
+        sqs.sendMessage(SendMessageRequest.builder().queueUrl(createQueueResponse.queueUrl()).messageBody("some message").messageAttributes(tempMap).build());
 
 
         ListQueuesResponse listQueuesResponse = sqs
